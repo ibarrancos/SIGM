c0522e2f93e474c651bdd9807e3e4ddb5fcfeca8
Ignacio Barrancos <ignacio.barrancos@carm.es>
2016-12-17 13:57:03 +0100

    Revisión del modulo sigem_registroPresencial a M3 Reviso los ficheros que cambian entre la versión 3.0.1-M2 y -M3

--- sigem/SIGEM_RegistroPresencial_Modules/SIGEM_RegistroPresencial/src/main/java/ieci/tecdoc/sgm/registropresencial/autenticacion/Login.java
+++ sigem/SIGEM_RegistroPresencial_Modules/SIGEM_RegistroPresencial/src/main/java/ieci/tecdoc/sgm/registropresencial/autenticacion/Login.java
@@ -1,5 +1,7 @@
 package ieci.tecdoc.sgm.registropresencial.autenticacion;
 
+import com.ieci.tecdoc.idoc.decoder.dbinfo.LDAPDef;
+import org.apache.log4j.Logger;
 
 import com.ieci.tecdoc.isicres.session.security.SecuritySession;
 import com.ieci.tecdoc.common.exception.SecurityException;
@@ -8,6 +10,8 @@ import com.ieci.tecdoc.common.exception.ValidationException;
 import es.ieci.tecdoc.fwktd.core.spring.configuration.jdbc.datasource.MultiEntityContextHolder;
 
 public class Login {
+
+	private static final Logger log = Logger.getLogger((Class)Login.class);
 
 	public static String login(User user, String entidad)
 			throws SecurityException, ValidationException {
@@ -18,5 +22,19 @@ public class Login {
 		MultiEntityContextHolder.setEntity(entidad);
 
 		return sessionID;
+	}
+
+	public static boolean isAuthenticationLdap(String entidad) {
+	    boolean result = false;
+	    try {
+	        LDAPDef ldapDef = SecuritySession.ldapInfo((String)entidad);
+	        if (ldapDef.getLdapEngine() != 0) {
+	            result = true;
+	        }
+	    }
+	    catch (Exception e) {
+	        log.warn((Object)"Se han encontrado problemas para obtener la pol\u00edtica de autenticaci\u00f3n", (Throwable)e);
+	    }
+	    return result;
 	}
 }
